954c0c51e59df983a299982ebb4c8c4ec03ea028
Marwinkas <marwinkas@mail.ru>
2024-09-13 08:50:20 +0300

    Задание 1

--- laravel-project/routes/web.php
+++ laravel-project/routes/web.php
@@ -2,12 +2,9 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('users', function()
+Route::get('/', function()
 {
-	return view('greeting',["product" => [
+	return view('ProductPage',["products" => [
         ["name" => "Orange", "cost" => 50000000, "amount" => 27],
         ["name" => "Banana", "cost" => 120000000, "amount" => 17],
         ["name" => "Bread", "cost" => 70000000, "amount" => 0]]]);
